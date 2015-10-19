96ba25ab0f39ea9f7a6ca1a4a948839d7d214b11
Darren Ackers <darren.ackers@mccann.com>
2015-10-19 18:07:51 +0100

    Fixed reference issues on the Api tests

--- Src/SkyTechnicalTest.Api.Tests/Properties/AssemblyInfo.cs
+++ Src/SkyTechnicalTest.Api.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkyTechnicalTest.Tests")]
+[assembly: AssemblyTitle("SkyTechnicalTest.Api.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SkyTechnicalTest.Tests")]
+[assembly: AssemblyProduct("SkyTechnicalTest.Api.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1b3709e8-d1e6-4c93-8a3c-4a31a8edbe0d")]
+[assembly: Guid("c304537f-8031-449c-9419-6299dd4399e3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
