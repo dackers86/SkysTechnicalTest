f7e336d98f6d2c5d78351b608d5a9a55f49d9531
Darren Ackers <ackers86@hotmail.com>
2015-10-31 15:18:05 +0000

    Added angular testing

---
+++ Src/SkyTechnicalTest.Web/client/tests/billing/billingServiceSpec.js
@@ -0,0 +1,40 @@
+describe('BillingService', function() {
+
+	var _billingService, _httpBackend;
+
+		beforeEach(module('skyBillingApp'));
+
+		beforeEach(inject(function (billingService, $httpBackend) {
+		    _billingService = billingService;
+		    _httpBackend = $httpBackend;
+	  	}));
+
+		afterEach(function() {
+		    _httpBackend.verifyNoOutstandingExpectation();                                                                                                                             
+		    _httpBackend.verifyNoOutstandingRequest();                                                                                                                                 
+		}); 
+
+		it('Should successfully get data from the billing Api', function() {
+
+			var expected = { "statement": {
+				"generated": "2015-01-11",
+				"due": "2015-01-25",
+				"period": {
+					"from": "2015-01-26",
+					"to": "2015-02-25"	
+					}
+				}
+			}
+						
+			_httpBackend.whenGET('http://api.skytechnicaltest.com/billing/1')
+			.respond(200, expected);
+
+			_billingService.Get().then(function(data){
+				expect(expected.generated).toBe(data.generated);
+			});
+
+			_httpBackend.flush();
+
+		});
+	
+});
